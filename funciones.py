01f569b4241e5d6020f2105571387ed207ecaa4a
enderfire99 <nachoqmt@gmail.com>
2024-06-26 20:36:52 -0300

    Arreglo de puntos del 1 al 6, falta 7 y 8

--- funciones.py
+++ funciones.py
@@ -88,43 +88,43 @@ def arreglo_observacional(x):
       return "ASADO"
   return x
 
-consumidores=consumidores_original.applymap(Mayuscula)
-nutricional=nutricional_original.applymap(Mayuscula)
+consumidores_original=consumidores_original.map(Mayuscula)
+nutricional_original=nutricional_original.map(Mayuscula)
 
-consumidores=consumidores.applymap(sacar_acentos)
-nutricional=nutricional.applymap(sacar_acentos)
+consumidores_original=consumidores_original.map(sacar_acentos)
+nutricional_original=nutricional_original.map(sacar_acentos)
 
 #Llenado de Ceros
-consumidores=consumidores.fillna(0)
-nutricional=nutricional.fillna(0)
+consumidores_original=consumidores_original.fillna(0)
+nutricional_original=nutricional_original.fillna(0)
 
 #Cambio todos los datos en unidad de gramos
-nutricional[['Na (mg)', 'Ca (mg)', 'Fe (mg)']] = nutricional[['Na (mg)', 'Ca (mg)', 'Fe (mg)']] / 1000
-nutricional = nutricional.rename(columns={'Na (mg)': 'Na (gr)','Ca (mg)':'Ca (gr)','Fe (mg)':'Fe (gr)'})
+nutricional_original[['Na (mg)', 'Ca (mg)', 'Fe (mg)']] = nutricional_original[['Na (mg)', 'Ca (mg)', 'Fe (mg)']] / 1000
+nutricional_original = nutricional_original.rename(columns={'Na (mg)': 'Na (gr)','Ca (mg)':'Ca (gr)','Fe (mg)':'Fe (gr)'})
 
 #Agregado de Verduras/Frutas
 #codigo binario 1= verdura o fruta y 0 no lo es
 FrutasyVerduras=['ACELGA','ZANAHORIA','TOMATE','LECHUGA','CEBOLLA','ZAPALLO','MANZANA','NARANJA','MANDARINA','PERA','BANANA','PAPA','BATATA']
 Verdura_fruta= []
 
-for index, fila in nutricional.iterrows():
+for index, fila in nutricional_original.iterrows():
     if fila['Alimento'] in FrutasyVerduras:
         Verdura_fruta.append(1)
     else:
         Verdura_fruta.append(0)
 
 # Agregamos la columna 'Verdura/Fruta' al DataFrame
-nutricional['Verdura/Fruta'] = Verdura_fruta
+nutricional_original['Verdura/Fruta'] = Verdura_fruta
 
 
 #Agrego unidades a consumidores
-consumidores = consumidores.rename(columns={'Cantidad': 'Cantidad (gr)'})
+consumidores_original = consumidores_original.rename(columns={'Cantidad': 'Cantidad (gr)'})
 
 def tabla_nutricional():
-    return nutricional
+    return nutricional_original
 
 def tabla_consumidores():
-    return consumidores
+    return consumidores_original
 
 def chequeoDieta(data):
     chequeo = True
@@ -162,49 +162,60 @@ def chequeoDieta(data):
     VF = sum(data_VF["Cantidad (gr/ml)"])
         
     #Chequeos   
-    print("HC")
-    print(minHC, HC, maxHC) 
+    #print("HC")
+    #print(minHC, HC, maxHC) 
     if minHC > HC or HC > maxHC:
         chequeo = False
-        print("False")
+        print("Los Hidratos de Carbono no cumplen los estandares de la OMS")
         
-    print()
-    print("Proteina")
-    print(minProteina, proteina, maxProteina) 
+    #print("Proteina")
+    #print(minProteina, proteina, maxProteina) 
     if minProteina > proteina or proteina > maxProteina:
         chequeo = False
-        print("False")
+        print("Las Proteinas no cumplen los estandares de la OMS")
         
-    print()
-    print("Grasas")
-    print(minGrasas, grasas, maxGrasas) 
+    #print("Grasas")
+    #print(minGrasas, grasas, maxGrasas) 
     if minGrasas > grasas or grasas > maxGrasas:
         chequeo = False
-        print("False")
+        print("Las Grasas no cumplen los estandares de la OMS")
+
         
-    print()
-    print("Sodio")
-    print(Na," > ", 0.2)
+    #print("Sodio")
+    #print(Na," > ", 0.2)
     if Na  < 0.2:
         chequeo = False
-        print("False")
+        print("El Sodio no cumple los estandares de la OMS")
         
         
-    print()
-    print("Fibra")
-    print(fibra," > ", 25) 
+    #print("Fibra")
+    #print(fibra," > ", 25) 
     if fibra < 25:
         chequeo = False
-        print("False")
-        
-    print()
-    print("Fruta y Verduras")
-    print(VF, ">=", 400)
+        print("La Fibra no cumple los estandares de la OMS")
+
+    #print("Fruta y Verduras")
+    #print(VF, ">=", 400)
     if VF < 400:
         chequeo = False
-        print("False")
+        print("La cantidad de Frutas y Verduras no cumple los estandares de la OMS")
         
     return chequeo
+
+def graficosPCA(data):
+  display(
+  so.Plot()
+  .add(so.Dot(), data=data, x="Z1", y="Z2")
+  )
+  display(
+  so.Plot()
+  .add(so.Dot(), data=data, x="Z2", y="Z3")
+  )
+  display(
+  so.Plot()
+  .add(so.Dot(), data=data, x="Z1", y="Z3")
+  )
+  
 
 def graficosNutricionales(data):
   #HC
@@ -229,7 +240,7 @@ def graficosNutricionales(data):
   Gr__Nutricional = Gr__Nutricional.rename(columns={"Precio por gr ($)":"Precio por gr de Grasas ($)"})
 
   display(
-    so.Plot(data, x="Fecha", y="Precio por gr ($)", color="Alimento")
+    so.Plot(data=data, x="Fecha", y="Precio por gr ($)", color="Alimento")
     .add(so.Dot())
     .add(so.Line(linewidth=1), so.PolyFit(1))
   )
@@ -273,65 +284,92 @@ def comparacionAumentos(data):
   # Inicializamos un modelo de Regresion Lineal
   modelo = linear_model.LinearRegression()
   
-  for i in range(0,4):
-    if i == 0:
-      #Calculo de Minimos Cuadrados
-      modelo.fit(data[['Fecha']], data[['Precio por gr ($)']])
-      
-      #Coeficientes
-      beta_1 = modelo.coef_[0][0]   # Con .coef_ recuperamos el valor de beta_1 (dentro de un array)
-      beta_0 = modelo.intercept_[0]   # Con .intercept_ recuperamos el valor de beta_0 (dentro de  un array)
-
-      for fecha in fechas:
-        predicion.loc[len(predicion.index)] = ["Todos",dt.date.fromordinal(fecha), beta_1*fecha+beta_0] 
+  for j in range(0,4):
+    if j == 0:
+      precio = "Precio por gr ($)"
+      nutriente = ""
+    if j == 1:
+      precio = "Precio por gr de HC ($)"
+      nutriente = "HC (gr)"
+      excluidos = ["Proteinas (gr)", "Grasas (gr)"]
+    if j == 2:
+      precio = "Precio por gr de Proteinas ($)"
+      nutriente = "Proteinas (gr)"
+      excluidos = ["HC (gr)", "Grasas (gr)"]
+    if j == 3:
+      precio = "Precio por gr de Grasas ($)"
+      nutriente = "Grasas (gr)"
+      excluidos = ["Proteinas (gr)", "HC (gr)"]
+  
+    predicion = pd.DataFrame(columns=["Rubro","Fecha", precio])
     
-      data['Fecha']=data['Fecha'].map(dt.date.fromordinal)
-
-    if i == 3:
-      #Calculo de Minimos Cuadrados
-      modelo.fit(carne_nutricional[['Fecha']], carne_nutricional[['Precio por gr ($)']])
+    if j == 0:
+      for i in range(0,4):
+        if i == 0:
+          data_analizar = data
+          rubro = "Todos"
+        if i == 1:
+          data_analizar = carne_nutricional
+          rubro = "Carnes"
+        if i == 2:
+          data_analizar = VF_nutricional
+          rubro = "Frutas y Verduras"
+        if i == 3:
+          data_analizar = almacen_nutricional
+          rubro = "Almacen"
       
-      #Coeficientes
-      beta_1 = modelo.coef_[0][0]   # Con .coef_ recuperamos el valor de beta_1 (dentro de un array)
-      beta_0 = modelo.intercept_[0]   # Con .intercept_ recuperamos el valor de beta_0 (dentro de  un array)
+        #Calculo de Minimos Cuadrados
+        modelo.fit(data_analizar[['Fecha']], data_analizar[[precio]])
+        
+        #Coeficientes
+        beta_1 = modelo.coef_[0][0]   # Con .coef_ recuperamos el valor de beta_1 (dentro de un array)
+        beta_0 = modelo.intercept_[0]   # Con .intercept_ recuperamos el valor de beta_0 (dentro de  un array)
 
-      for fecha in fechas:
-        predicion.loc[len(predicion.index)] = ["Carne",dt.date.fromordinal(fecha), beta_1*fecha+beta_0] 
+        for fecha in fechas:
+          predicion.loc[len(predicion.index)] = [rubro,dt.date.fromordinal(fecha), beta_1*fecha+beta_0] 
+
+      display(
+        so.Plot(data=predicion, x="Fecha", y=precio, color="Rubro")
+        .add(so.Line(linewidth=2), so.PolyFit(1))
+      )
+      
+    if j > 0:
+      for i in range(0,4):
+        if i == 0:
+          data_analizar = data
+          rubro = "Todos"
+        if i == 1:
+          data_analizar = carne_nutricional
+          rubro = "Carnes"
+        if i == 2:
+          data_analizar = VF_nutricional
+          rubro = "Frutas y Verduras"
+        if i == 3:
+          data_analizar = almacen_nutricional
+          rubro = "Almacen"
+        
+        HC__Nutricional = data_analizar[data_analizar[nutriente] > 0].drop(columns=excluidos).reset_index(drop=True)
+
+        if len(HC__Nutricional) == 0:
+          continue
+
+        HC__Nutricional["Precio por gr ($)"] = HC__Nutricional["Precio por gr ($)"] / HC__Nutricional[nutriente]
+
+        HC__Nutricional = HC__Nutricional.rename(columns={"Precio por gr ($)":precio})
+        
+        #Calculo de Minimos Cuadrados
+        modelo.fit(HC__Nutricional[['Fecha']], HC__Nutricional[[precio]])
+        
+        #Coeficientes
+        beta_1 = modelo.coef_[0][0]   # Con .coef_ recuperamos el valor de beta_1 (dentro de un array)
+        beta_0 = modelo.intercept_[0]   # Con .intercept_ recuperamos el valor de beta_0 (dentro de  un array)
+
+        for fecha in fechas:
+          predicion.loc[len(predicion.index)] = [rubro,dt.date.fromordinal(fecha), beta_1*fecha+beta_0] 
+        
+      display(
+        so.Plot(data=predicion, x="Fecha", y=precio, color="Rubro")
+        .add(so.Line(linewidth=2), so.PolyFit(1))
+      )
     
-      carne_nutricional['Fecha']=carne_nutricional['Fecha'].map(dt.date.fromordinal)
-    
-    if i == 2:
-      #Calculo de Minimos Cuadrados
-      modelo.fit(VF_nutricional[['Fecha']], VF_nutricional[['Precio por gr ($)']])
-      
-      #Coeficientes
-      beta_1 = modelo.coef_[0][0]   # Con .coef_ recuperamos el valor de beta_1 (dentro de un array)
-      beta_0 = modelo.intercept_[0]   # Con .intercept_ recuperamos el valor de beta_0 (dentro de  un array)
-
-      for fecha in fechas:
-        predicion.loc[len(predicion.index)] = ["Frutas y Verduras",dt.date.fromordinal(fecha), beta_1*fecha+beta_0] 
-
-      VF_nutricional['Fecha']=VF_nutricional['Fecha'].map(dt.date.fromordinal)
-
-
-    if i == 1:
-      #Calculo de Minimos Cuadrados
-      modelo.fit(almacen_nutricional[['Fecha']], almacen_nutricional[['Precio por gr ($)']])
-      
-      #Coeficientes
-      beta_1 = modelo.coef_[0][0]   # Con .coef_ recuperamos el valor de beta_1 (dentro de un array)
-      beta_0 = modelo.intercept_[0]   # Con .intercept_ recuperamos el valor de beta_0 (dentro de  un array)
-
-      for fecha in fechas:
-        predicion.loc[len(predicion.index)] = ["Almacen",dt.date.fromordinal(fecha), beta_1*fecha+beta_0] 
-     
-      almacen_nutricional['Fecha']=almacen_nutricional['Fecha'].map(dt.date.fromordinal)
-
-  display(
-    so.Plot(data=predicion, x="Fecha", y="Precio por gr ($)", color="Rubro")
-    .add(so.Line(linewidth=2), so.PolyFit(1))
-  )
-
-  return beta_1, beta_0
-
 
